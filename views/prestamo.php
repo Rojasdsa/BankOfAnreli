b72db9f93853452ec67949a64d5ad9dbc4ae51dc
Fernando Rojas <rojasdsa@alumnos.ilernasevilla.com>
2023-12-14 14:44:25 +0100

    secciones principales de la cuenta con includes header/footer

---
+++ views/prestamo.php
@@ -0,0 +1,9 @@
+
+<?php include_once("../includes/header.php"); ?>
+
+<main>
+  <h2>Préstamos</h2>
+  <!-- lógica de PHP -->
+</main>
+
+<?php include_once("../includes/footer.php"); ?>
---
+++ views/profile.php
@@ -0,0 +1,9 @@
+
+<?php include_once("../includes/header.php"); ?>
+
+<main>
+  <h2>Perfil</h2>
+  <!-- lógica de PHP -->
+</main>
+
+<?php include_once("../includes/footer.php"); ?>
